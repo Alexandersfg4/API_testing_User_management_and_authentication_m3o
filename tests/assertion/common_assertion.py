7618db35dbd0c56b1b010f33439ccaecbcd3856d
Alexander <alexanderbortnikov79@gmail.com>
2021-11-11 19:07:05 +0300

    Rafactoring has been completed

--- tests/assertion/common_assertion.py
+++ tests/assertion/common_assertion.py
@@ -1,6 +1,6 @@
 
 
-def assert_status_codes_are_matched(actual_code, excepted_code):
+def assert_status_codes_are_matched(excepted_code, actual_code):
     assert actual_code == excepted_code, f'{excepted_code} is not mathced with {actual_code}'
         
         
@@ -12,4 +12,12 @@ def assert_parameters_of_created_user_are_corrected(read_playload, created_playl
     assert read_playload["id"] == created_playload["id"], "Read id != created id"
     # Delay should be less than 5 sec
     assert created_playload["timestamp"] + 5 >= int(read_playload["created"]) >= created_playload["timestamp"], "Creation time is not correct"
-    assert created_playload["timestamp"] + 5 >= int(read_playload["updated"]) >= created_playload["timestamp"], "Updation time is not correct"        
+    assert created_playload["timestamp"] + 5 >= int(read_playload["updated"]) >= created_playload["timestamp"], "Updation time is not correct" 
+    
+def assert_that_updation_has_effects(updated_playload, read_playload):
+    read_playload = read_playload["account"]
+    assert read_playload["username"] == updated_playload["username"], "Read username != created username"
+    assert read_playload["email"] == updated_playload["email"], "Read email != created email"
+    assert read_playload["created"] == updated_playload["created"], "Creation time is not correct" 
+    assert read_playload["updated"] > updated_playload["updated"], "Updation is not changed" 
+           
--- tests/user_management_test.py
+++ tests/user_management_test.py
@@ -29,11 +29,11 @@ def test_delete_user():
     assert_status_codes_are_matched(500, status_code)
     
 def test_update_user_data():
-    created_playload, status_code = client.create_user()
-    assert_status_codes_are_matched(200, status_code)
+    created_playload = data.get_playloads('test_update_user_data')[1]
     updated_playload = data.get_playloads('test_update_user_data')[0]
     playload, status_code = client.update_user_data(created_playload, updated_playload) 
     assert_status_codes_are_matched(200, status_code)
     read_playload, status_code = client.read_user(created_playload, read_by="id")
-    data.write_playload(read_playload, "read")
     assert_status_codes_are_matched(200, status_code)
+    assert_that_updation_has_effects(updated_playload, read_playload)
+    
